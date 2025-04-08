517fecfe59feeff40b1541debd56748cc1ba22c6
ohminsuk <omsc01060@postech.ac.kr>
2025-04-08 18:08:16 +0900

    && || 학습

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ console.log({name: "a", age: 10});
 
 let val1 = "A", val2 = "B";
 const con1 = 3.141592;
+let val3 = 3;
 console.log(val1, val2, con1);
+console.log(typeof val1);
+console.log(typeof con1);
+console.log(typeof val3);
+
+console.log(
+    typeof 'a', isNaN('a'), Number.isNaN('a')
+  ); // ⚠️ 특정 숫자로 변환 불가인 문자의 경우 차이
+
+console.log(
+    typeof (1/'a'), isNaN(1/'a'), Number.isNaN(1/'a')
+); // NaN값인 경우
+
+console.log("result", true && console.log("inner output")) // A && B는 A가 true면 B반환(ㄹㅇ 그대로 반환)
